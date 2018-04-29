aaf75e5242d730e70e61a349cd316304ac6bb62f
Ramil Musaev <r.musaev@astrio.net>
2018-04-29 16:10:32 +0400

    filter before map

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -5,26 +5,28 @@ const toString = (body) => {
   return `value: '${body}'`;
 };
 
-const renderPlain = (diff, parentKey = '') => diff.map((element) => {
-  const {
-    key,
-    to,
-    from,
-    children,
-  } = element;
+const renderPlain = (diff, parentKey = '') => diff
+  .filter(node => node.type !== 'same')
+  .map((element) => {
+    const {
+      key,
+      to,
+      from,
+      children,
+    } = element;
 
-  switch (element.type) {
-    case 'added':
-      return `Property '${parentKey}${key}' was added with ${toString(to)}`;
-    case 'deleted':
-      return `Property '${parentKey}${key}' was removed`;
-    case 'modified':
-      return `Property '${parentKey}${key}' was updated. From ${toString(from)} to ${toString(to)}`;
-    case 'inserted':
-      return renderPlain(children, `${parentKey}${key}.`);
-    default:
-      return element;
-  }
-}, []).filter(node => typeof node === 'string').join('\n');
+    switch (element.type) {
+      case 'added':
+        return `Property '${parentKey}${key}' was added with ${toString(to)}`;
+      case 'deleted':
+        return `Property '${parentKey}${key}' was removed`;
+      case 'modified':
+        return `Property '${parentKey}${key}' was updated. From ${toString(from)} to ${toString(to)}`;
+      case 'inserted':
+        return renderPlain(children, `${parentKey}${key}.`);
+      default:
+        return element;
+    }
+  }, []).join('\n');
 
 export default renderPlain;
